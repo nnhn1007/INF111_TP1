2ba0586879b4a758afa4588603e33205884ef38d
mathisada <mathis-ada@hotmail.com>
2024-10-22 10:01:35 -0400

    Question 2.1

---
+++ BankServer/BankServer/src/main/java/com/atoudeft/banque/CompteCheque.java
@@ -0,0 +1,34 @@
+package com.atoudeft.banque;
+
+public class CompteCheque extends CompteBancaire{
+    /**
+     * Crée un compte chèque.
+     *
+     * @param numero numéro du compte
+     * @param type   type du compte
+     */
+    public CompteCheque(String numero, TypeCompte type) {
+        super(numero, type);
+    }
+
+    @Override
+    public boolean crediter(double montant) {
+
+        return false;
+    }
+
+    @Override
+    public boolean debiter(double montant) {
+        return false;
+    }
+
+    @Override
+    public boolean payerFacture(String numeroFacture, double montant, String description) {
+        return false;
+    }
+
+    @Override
+    public boolean transferer(double montant, String numeroCompteDestinataire) {
+        return false;
+    }
+}
